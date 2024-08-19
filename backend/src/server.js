e96950d17164cbbc257c0926ce594522b677d925
trangiathuan <nakrothfaker@gmail.com>
2024-08-19 10:51:15 +0700

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,11 @@ const commentRoutes = require('./routes/commentRoutes');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({
+    origin: ["https://dacn-seeds.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"]
+
+}))
 app.use(bodyParser.json());
 app.use('/', productRoutes)
 app.use('/', categoryRoutes)
